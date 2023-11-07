c43b87c6a4681b4415238366c2736f2a9b117db5
RadKesvat <134321679+radkesvat@users.noreply.github.com>
2023-11-08 02:02:13 +0330

    create libnet context

--- src/main.c
+++ src/main.c
@@ -1,101 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <libgen.h>
-#include <errno.h>
-#include <string.h>
-#include <getopt.h>
+#include <libnet.h>
 
-#define OPTSTR "vi:o:f:h"
-#define USAGE_FMT  "%s [-v] [-f hexflag] [-i inputfile] [-o outputfile] [-h]"
-#define ERR_FOPEN_INPUT  "fopen(input, r)"
-#define ERR_FOPEN_OUTPUT "fopen(output, w)"
-#define ERR_DO_THE_NEEDFUL "do_the_needful blew up"
-#define DEFAULT_PROGNAME "george"
+int main() {
 
-extern int errno;
-extern char *optarg;
-extern int opterr, optind;
+  libnet_t *l;  /* the libnet context */
+  char errbuf[LIBNET_ERRBUF_SIZE];
 
-typedef struct {
-  int           verbose;
-  uint32_t      flags;
-  FILE         *input;
-  FILE         *output;
-} options_t;
+  l = libnet_init(LIBNET_RAW4, NULL, errbuf);
+  if ( l == NULL ) {
+    fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
+    exit(EXIT_FAILURE);
+  }
 
-int dumb_global_variable = -11;
-
-void usage(char *progname, int opt);
-int  do_the_needful(options_t *options);
-
-int main(int argc, char *argv[]) {
-    int opt;
-    options_t options = { 0, 0x0, stdin, stdout };
-
-    opterr = 0;
-
-    while ((opt = getopt(argc, argv, OPTSTR)) != EOF) 
-       switch(opt) {
-           case 'i':
-              if (!(options.input = fopen(optarg, "r")) ){
-                 perror(ERR_FOPEN_INPUT);
-                 exit(EXIT_FAILURE);
-                 /* NOTREACHED */
-              }
-              break;
-
-           case 'o':
-              if (!(options.output = fopen(optarg, "w")) ){
-                 perror(ERR_FOPEN_OUTPUT);
-                 exit(EXIT_FAILURE);
-                 /* NOTREACHED */
-              }    
-              break;
-              
-           case 'f':
-              options.flags = (uint32_t )strtoul(optarg, NULL, 16);
-              break;
-
-           case 'v':
-              options.verbose += 1;
-              break;
-
-           case 'h':
-           default:
-              usage(basename(argv[0]), opt);
-              /* NOTREACHED */
-              break;
-       }
-
-    if (do_the_needful(&options) != EXIT_SUCCESS) {
-       perror(ERR_DO_THE_NEEDFUL);
-       exit(EXIT_FAILURE);
-       /* NOTREACHED */
-    }
-
-    return EXIT_SUCCESS;
-}
-
-void usage(char *progname, int opt) {
-   fprintf(stderr, USAGE_FMT, progname?progname:DEFAULT_PROGNAME);
-   exit(EXIT_FAILURE);
-   /* NOTREACHED */
-}
-
-int do_the_needful(options_t *options) {
-
-   if (!options) {
-     errno = EINVAL;
-     return EXIT_FAILURE;
-   }
-
-   if (!options->input || !options->output) {
-     errno = ENOENT;
-     return EXIT_FAILURE;
-   }
-
-   /* XXX do needful stuff */
-
-   return EXIT_SUCCESS;
+  libnet_destroy(l);
+  return 0;
 }
